7bfe5f459c55241fa9f1025284c312a0e9798dbf
Ailyinchick <Ailyinchick@gmail.com>
2020-04-13 16:52:10 +0300

    Fixed local cache to separate it from DB

--- src/main/java/pack/controller/HomeController.java
+++ src/main/java/pack/controller/HomeController.java
@@ -37,99 +37,111 @@ public class HomeController {
     }
 
     String refreshData(ModelMap modelMap) {
-        modelMap.addAttribute("picture", lifetimeChecker.findTheYoundest().getImage());
         modelMap.addAttribute("cache", lifetimeChecker.getMap().keySet());
         modelMap.addAttribute("count", lifetimeChecker.getMap().size());
+        modelMap.addAttribute("picture", lifetimeChecker.findTheYoundest().getImage());
         return homePage(modelMap);
+    }
+
+    Car getCar(String carName) {
+        Car ansCar;
+        if (lifetimeChecker.getMap().keySet().stream().anyMatch(car -> car.getName().equals(carName))) {
+            ansCar = lifetimeChecker.getMap().keySet().stream().filter(car -> car.getName().equals(carName)).findFirst().get();
+        } else {
+            ansCar = carRepository.findCarByName(carName);
+        }
+        lifetimeChecker.addToCache(ansCar);
+        return ansCar;
     }
 
     @GetMapping(value = "/audi_quattro")
     public String quattro(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("audi_quattro"));
+        modelMap.addAttribute("car", getCar("audi_quattro"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/x6")
     public String x6(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("x6"));
+        modelMap.addAttribute("car", getCar("x6"));
+
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/bmw_x5")
     public String bmwx5(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("bmw_x5"));
+        modelMap.addAttribute("car", getCar("bmw_x5"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/camaro_ss")
     public String camaro(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("camaro_ss"));
+        modelMap.addAttribute("car", getCar("camaro_ss"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/eclipse")
     public String eclipse(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("eclipse"));
+        modelMap.addAttribute("car", getCar("eclipse"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/eleanor")
     public String eleanor(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("eleanor"));
+        modelMap.addAttribute("car", getCar("eleanor"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/EXO_X")
     public String evo_x(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("EXO_X"));
+        modelMap.addAttribute("car", getCar("EXO_X"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/focus")
     public String focus(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("focus"));
+        modelMap.addAttribute("car", getCar("focus"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/gt500")
     public String gt500(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("gt500"));
+        modelMap.addAttribute("car", getCar("gt500"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/lanciaB")
     public String lanciaB(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("lanciaB"));
+        modelMap.addAttribute("car", getCar("lanciaB"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/loncolnTC")
     public String loncolnTC(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("loncolnTC"));
+        modelMap.addAttribute("car", getCar("loncolnTC"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/r34")
     public String r34(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("r34"));
+        modelMap.addAttribute("car", getCar("r34"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/rx8")
     public String rx8(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("rx8"));
+        modelMap.addAttribute("car", getCar("rx8"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/WRX_STI")
     public String WRX(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("WRX_STI"));
+        modelMap.addAttribute("car", getCar("WRX_STI"));
         return refreshData(modelMap);
     }
 
     @GetMapping(value = "/xc90")
     public String xc90(ModelMap modelMap) {
-        lifetimeChecker.addToCache(carRepository.findCarByName("xc90"));
+        modelMap.addAttribute("car", getCar("xc90"));
         return refreshData(modelMap);
     }
 
--- src/main/java/pack/dao/CarRepository.java
+++ src/main/java/pack/dao/CarRepository.java
@@ -18,12 +18,7 @@ public class CarRepository implements DaoRepos<Car> {
     }
 
     public Car findCarByName(String carName) {
+        System.out.println("Лезем в БД");
         return findAll(Car.class, sessionFactory).stream().filter(car -> car.getName().equals(carName)).findFirst().get();
-/*        Car car = new Car();
-        for (Car iteratingCar : findAll(Car.class, sessionFactory)
-        ) {
-            if (iteratingCar.getName().equals(carName)) car = iteratingCar;
-        }
-        return car;*/
     }
 }
