7c393d92da697be9b9c5fc736d4c6a8763f9e228
Ailyinchick <Ailyinchick@gmail.com>
2020-04-20 12:01:56 +0300

    Added SQL file to insert values in table

---
+++ src/main/resources/insertCars.sql
@@ -0,0 +1,20 @@
+use forcache;
+
+SELECT * FROM forcache.car;
+
+insert into car values
+(1, "bmw x5", "<img src=\"resources/images/bmw-x5.jpg\" alt=\"bmw-x5\">"),
+(2, "audi quattro", "<img src=\"resources/images/audiQuattroB.jpg\" alt=\"audiQuattroB\">"),
+(3, "camaro-ss", "<img src=\"resources/images/camaro-ss.jpg\" alt=\"camaro-ss\">"),
+(4, "eclipse", "<img src=\"resources/images/eclipse.jpg\" alt=\"eclipse\">"),
+(5, "eleanor", "<img src=\"resources/images/eleanor.jpg\" alt=\"eleanor\">"),
+(6, "EXO-X", "<img src=\"resources/images/EXO-X.jpg\" alt=\"EXO-X\">"),
+(7, "focus", "<img src=\"resources/images/focus.jpg\" alt=\"focus\">"),
+(8, "gt500", "<img src=\"resources/images/gt500.jpg\" alt=\"gt500\">"),
+(9, "lanciaB", "<img src=\"resources/images/lanciaB.jpg\" alt=\"lanciaB\">"),
+(10, "loncolnTC", "<img src=\"resources/images/loncolnTC.jpg\" alt=\"loncolnTC\">"),
+(11, "r34", "<img src=\"resources/images/r34.jpg\" alt=\"r34\">"),
+(12, "rx8", "<img src=\"resources/images/rx8.jpg\" alt=\"rx8\">"),
+(13, "WRX-STI", "<img src=\"resources/images/WRX-STI.jpg\" alt=\"WRX-STI\">"),
+(14, "x6", "<img src=\"resources/images/x6.jpg\" alt=\"x6\">"),
+(15, "xc90", "<img src=\"resources/images/xc90.jpg\" alt=\"xc90\">");
